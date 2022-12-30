e4ae10b189ec5201a99194a966381c117e1548ee
mdshahbajalam7 <98772205+mdshahbajalam7@users.noreply.github.com>
2022-12-30 19:26:08 +0530

    summary update

--- Mdport/index.js
+++ Mdport/index.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["MARN Stack  Developer", "Developer",],
+        strings: ["MERN Stack  Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["MARN Stack  Developer", "Developer",],
+        strings: ["MERN Stack  Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
